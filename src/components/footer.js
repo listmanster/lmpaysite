bc9bd9010042dbc7db4777a913e63dae5efa2444
listmanster <listmanster@gmail.com>
2020-10-13 17:36:37 +0530

    Updating pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,74 +1,23 @@
 import React from 'react';
-import logo from '../css/logo.png';
+import logo from '../css/logo_text.png';
 
 const Footer = () => {
 return (<footer className="bg-white ">
-      <div className="container mx-auto mt-8 px-8">
-        <div className="w-full flex flex-col md:flex-row py-6">
-          <div className="flex-1 mb-6">
-          <img className="h-8 w-8 mr-3 mb-2 inline-block fill-current text-orange-700"  src={logo} alt="ParserBase logo" />
-                {/* <svg className="h-6 w-6 inline-block fill-current text-orange-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M13 8V0L8.11 5.87 3 12h4v8L17 8h-4z"></path>
-                </svg> */}
-                ParserBase
-          </div>
-
-          <div className="flex-1">
-            <p className="uppercase font-extrabold text-gray-500 md:mb-6">Links</p>
+      <div className="text-center">
             <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">FAQ</a>
+              <li className="inline-block text-center px-4 py-2 m-2">
+              <img className="h-12 w-auto mr-3 mb-2 inline-block"  src={logo} alt="ParserBase logo" />
               </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Help</a>
+              <li className="inline-block text-center px-4 py-2 m-2">
+                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Policies</a>
               </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+              <li className="inline-block text-center px-4 py-2 m-2">
+                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">About</a>
+              </li>
+              <li className="inline-block text-center px-4 py-2 m-2">
                 <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Support</a>
               </li>
             </ul>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase font-extrabold text-gray-500 md:mb-6">Legal</p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Terms</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Privacy</a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase font-extrabold text-gray-500 md:mb-6">Social</p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Facebook</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Linkedin</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Twitter</a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase font-extrabold text-gray-500 md:mb-6">
-              Company
-            </p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Official Blog</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">About Us</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="font-light no-underline hover:underline text-gray-800 hover:text-orange-500">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
       </div>
     </footer>    
 );
---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {ContentLayout} from '../components/layout';
+
+
+
+
+
+export default () => {
+    return (
+        <ContentLayout>
+            <div>
+                <h1>404</h1>
+            </div>
+        </ContentLayout>
+    );
+}
